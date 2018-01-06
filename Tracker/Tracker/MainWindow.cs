b2b24f9a28fa7aaf8a27444785f724eae20432ec
Dave Crosslin <concertguitar327@gmail.com>
2018-01-06 15:10:33 -0500

    first commit

---
+++ Tracker/Tracker/MainWindow.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Gtk;
+using MySql.Data.MySqlClient;
+
+public partial class MainWindow: Gtk.Window
+{
+	
+	public MainWindow () : base (Gtk.WindowType.Toplevel)
+	{
+		
+		List<string> comboList = new List<string>();
+		comboList.Add ("Dave");
+	
+
+		Build ();
+	
+
+		foreach (string ID in comboList)
+		{
+			combobox1.AppendText (ID);
+
+		}
+
+	}
+
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+
+
+}
---
+++ Tracker/Tracker/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Gtk;
+using MySql.Data.MySqlClient;
+
+
+namespace Tracker
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Application.Init ();
+			MainWindow win = new MainWindow ();
+			win.Show ();
+			Application.Run ();
+
+		}
+	
+	}
+}
