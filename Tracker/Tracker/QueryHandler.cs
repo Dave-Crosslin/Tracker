91e1e31f1ea0815ec709f59a4a114b24cf882a2b
Dave Crosslin <concertguitar327@gmail.com>
2018-01-13 02:13:55 -0500

    Fourth commit

--- Tracker/Tracker/QueryHandler.cs
+++ Tracker/Tracker/QueryHandler.cs
@@ -10,22 +10,26 @@ namespace Tracker
 	{
 		MySqlCommand command = MySqlConnection.CreateCommand();
 
+		public void QueryCreate ()
+		{	
+			string query = "\"SELECT ID FROM Employees GROUP BY ID;\""
+		}
+
 		public void QueryExecute ()
 		{
+			
 			MySqlDataReader Reader;
 			Reader = command.ExecuteReader();
 
-			while (Reader.Read())
-			{
-				List<string> row = new List<string>();
-				for (int i = 0; i < Reader.FieldCount; i++)
-			{
+
 		}
-	
-				public void QueryCreate()
-		{	
-			MySqlCommand command = MySqlConnection.CreateCommand(" SELECT ID FROM Employees GROUP BY ID;");
+
+		public void QueryStore ()
+		{
+			//to write to strings, lists, GUI, ect.
 		}
 
 	}
+		
 }
+
