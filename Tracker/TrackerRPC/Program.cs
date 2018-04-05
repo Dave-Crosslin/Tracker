13f8d854e17bb1180fa79598bc614ee4888d4b2d
Dave Crosslin <concertguitar327@gmail.com>
2018-04-05 02:45:42 -0400

    removed dolder

---
+++ Tracker/TrackerRPC/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Core;
+using TrackerRPC;
+
+namespace TrackerServer
+{
+	class TrackerRPCImpl : dbUpdate.dbUpdateBase
+
+	{
+		// Server side handler of the dbUpdate RPC
+		public override Task<RecieptConfirmation> GetRow( UpdateRequest request, ServerCallContext context)
+		{
+			return Task.FromResult(new RecieptConfirmation { WasRecieved = true});
+
+		}
+	}
+
+	class MainClass
+	{
+		const int Port = 50051;
+		
+		public static void Main (string[] args)
+		{
+			Server server = new Server
+			{
+				Services = {dbUpdate.BindService(new TrackerRPCImpl()) },
+				Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+			};
+			server.Start();
+
+			Console.WriteLine ("Tracker Server listening on port " + Port);
+			Console.WriteLine("Press any key to stop the server...");
+
+		
+			Console.ReadKey();
+			
+			server.ShutdownAsync().Wait();
+
+
+		}
+			
+	}
+}
